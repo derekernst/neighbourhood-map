9edc1eedc7dfa8dde55c4898b3d3afe7f9f8fa57
Derek Ernst <mernst.derek@gmail.com>
2016-09-28 20:25:19 -0400

    Added map

--- scripts.js
+++ scripts.js
@@ -1,6 +1,4 @@
-var map;
-
-var initLocations = [
+var locationData = [
 	{
 		 title: 'Park Ave Penthouse',
 		 location: {lat: 40.7713024, lng: -73.9632393}
@@ -15,62 +13,56 @@ var initLocations = [
 	}
 ]
 
-var Markers = function (data) {
-	this.title = ko.observable(data.title);
-	//use observable to bind which locations are visible
-	this.visible = ko.observable(true);
-	//not observable since the location won't be changing?????
-	this.position = data.location;
-	//set marker for map
-	this.marker = null;
+var map;
+var infowindow;
 
+var initMap = function() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: {
+            lat: 43.653226,
+            lng: -79.38318429999998
+        }
+    });
+
+};
+
+var Marker = function (data) {
+	this.title = ko.observable(data.title);
+	this.position = data.location;
+	this.marker = null;
+	this.visible = ko.observable(true);
 }
 
 var ViewModel = function() {
 	var self = this;
 
-    //set the initial locations to an observable array
-	this.locationsList = ko.observableArray([]);
+	this.markers = ko.observableArray([]);
 
-	//push all initLocations to locationsList
-	initLocations.forEach(function(location) {
-		self.locationsList.push(new Markers(location));
+	locationData.forEach(function(location) {
+		self.markers().push(new Marker(location));
 	});
 
-	//set filter field for user input
+	// filter input
 	this.filter = ko.observable('');
 
-	// apply filter when enter key is hit
+	// filter markers
 	this.filterMarkers = function () {
 		var filter = this.filter().toUpperCase();
-		this.locationsList().forEach(function(location) {
-			if (location.title().toUpperCase().indexOf(filter) === -1){
-				location.visible(false);
+		this.markers().forEach(function(marker) {
+			if (marker.title().toUpperCase().indexOf(filter) === -1){
+				marker.visible(false);
 			} else {
-				location.visible(true);
+				marker.visible(true);
 			}
 		});
 	};
 
-	this.getMarkers = function() {
-
-	};
-
 }
-
-var initMap = function() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 6,
-        center: {
-            lat: 38.3401967,
-            lng: -81.6032057
-        }
-    });
-};
 
 var mapsError = function() {
   window.alert("ERROR!");
 }
 
-ko.applyBindings(new ViewModel);
+ko.applyBindings(new ViewModel());
 
