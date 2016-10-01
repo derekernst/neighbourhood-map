8a013a4f99154e6a4e05f19da1b076d0d1f808f5
Derek Ernst <mernst.derek@gmail.com>
2016-10-01 00:27:02 -0400

    Added marker highlights for list and map items

--- scripts.js
+++ scripts.js
@@ -3,15 +3,18 @@ var map;
 var locationData = [
 	{
 		 title: 'Rogers Centre',
-		 location: {lat: 43.6414378, lng: -79.38935320000002}
+		 location: {lat: 43.6414378, lng: -79.38935320000002},
+		 id: 0
 	},
 	{
 		 title: 'Casa Loma',
-		 location: {lat: 43.67803709999999, lng: -79.40944389999999}
+		 location: {lat: 43.67803709999999, lng: -79.40944389999999},
+		 id: 1
 	},
 	{
 		 title: 'Trinity Bellwoods Park',
-		 location: {lat: 43.6500371, lng: -79.41693559999999}
+		 location: {lat: 43.6500371, lng: -79.41693559999999},
+		 id: 2
 	}
 ]
 
@@ -19,14 +22,27 @@ var ListMarker = function (data) {
 	this.title = ko.observable(data.title);
 	this.position = data.location;
 	this.visible = ko.observable(true);
+	this.id = data.id;
+	this.active = ko.observable(false);
+	this.style = ko.computed(function() { 
+		if (this.active() === true) { 
+			return 'active-marker';
+		} else { 
+			return 'filter-item';
+		}
+	},this)
 };
 
 var ViewModel = function() {
 	var self = this;
-	// store the map markers
+
+	// var currentMarker = {};
+
+	// store the map markersList
 	this.markers = [];
 	this.markersList = ko.observableArray([]);
 	this.bounds = new google.maps.LatLngBounds();
+	this.infoWindow = new google.maps.InfoWindow();
 	
 	// push each location to the markersList array
 	locationData.forEach(function(location) {
@@ -36,7 +52,8 @@ var ViewModel = function() {
             map: map,
             position: location.location,
             title: location.title,
-            animation: google.maps.Animation.DROP
+            animation: google.maps.Animation.DROP,
+            id: location.id
           });
 		// hold each marker that was created in markers[]
 		self.markers.push(marker);
@@ -44,38 +61,40 @@ var ViewModel = function() {
 		self.bounds.extend(marker.position);
 		// click event on marker to bring up info windwo
 		marker.addListener('click', function() {
-			var infoWindow = new google.maps.InfoWindow();
-            self.populateInfoWindow(this, infoWindow);
+			// var infoWindow = new google.maps.InfoWindow();
+			toggleBounce(this);
+            populateInfoWindow(this, self.infoWindow);
+            activeListMarker(this);
          });
 	});
 	//fit map to the new boundaries of the markers
 	map.fitBounds(self.bounds);
 
+	//animate map marker when clicked on
+	var toggleBounce = function(marker) {
+		marker.setAnimation(google.maps.Animation.BOUNCE);
+		window.setTimeout(function() {
+			marker.setAnimation(null);
+		}, 2150);
+	};
+
 	// open info window for clicked marker
-	this.populateInfoWindow = function(marker, infowindow) {
-		// when we get to the API stuff, use marker 
-        if (infowindow.marker != marker) {
+	var populateInfoWindow = function(marker, infowindow) {
             infowindow.marker = marker;
             infowindow.setContent('<h3>' + marker.title + '</h3><p>this is an info window</p>');
             infowindow.open(map, marker);
-            // event for closing the info window --> was getting an error when using
-            // infowindow.addListener('closeclick', function() {
-            //   	infowindow.setMarker(null);
-            // });
-        }
     };
 
-	// add markers to map
-	// this.markers = [];
-	// locationData.forEach(function(location) { 
-	// 	var marker = new google.maps.Marker({
- //            map: map,
- //            position: location.location,
- //            title: location.title,
- //            animation: google.maps.Animation.DROP
- //          });
-	// 	self.markers.push(marker);
-	// });
+    var activeListMarker = function (marker) { 
+    	self.markersList().forEach(function(location) { 
+			// update list marker based on clicked map marker
+			if (location.id == marker.id) { 
+				location.active(true);
+			} else { 
+				location.active(false);
+			}
+		});
+    };
 
 	// filter markers
 	this.filter = ko.observable('');
@@ -107,11 +126,34 @@ var ViewModel = function() {
 		});
 	};
 
+	//highlight clicked list and map marker
+	this.setMarker = function(clickedMarker) { 
+		// markersList()
+		self.markersList().forEach(function(location) { 
+			//update list marker based on clicked item
+			if (location.id == clickedMarker.id) { 
+				location.active(true);
+			} else { 
+				location.active(false);
+			}
+		});
+		// markers -- for the map
+		self.markers.forEach (function (marker) { 
+			//open map marker based on clicked item
+			if (marker.id == clickedMarker.id) { 
+				toggleBounce(marker);
+				populateInfoWindow(marker, self.infoWindow);
+			} 
+		});
+	}
 }
+
+
+
 
 var initMap = function() {
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
+        zoom: 12,
         center: {
             lat: 43.653226,
             lng: -79.38318429999998
@@ -123,8 +165,4 @@ var initMap = function() {
 
 var mapsError = function() {
   window.alert("ERROR!");
-}
-
-
-
-
+};
